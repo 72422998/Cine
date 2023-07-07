b9a844f780a36a6dd50b9c6725d0363c41f5e4a5
DavidChambilla <anvor127@gmail.com>
2023-07-07 13:08:21 -0500

    feat: Producto
    Producto entity

---
+++ src/main/java/pe/com/cine/entities/Producto.java
@@ -0,0 +1,56 @@
+package pe.com.cine.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "producto_id")
+    private Long productoId;
+
+    @Column(name = "nombre")
+    @NotEmpty
+    @Size(max = 50)
+    private String nombre;
+    @Column(name = "precio")
+    @NotEmpty
+    @Size(max = 20)
+    private Double precio;
+
+    @ManyToOne
+    @JoinColumn(name = "confiteria_id")
+    private Confiteria confiteria;
+
+    public Producto(String nombre, Double precio,
+            Confiteria confiteria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.confiteria = confiteria;
+
+    public Producto(@NotEmpty @Size(max = 50) String nombre, @NotEmpty @Size(max = 20) Double precio,
+            Confiteria confiteria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.confiteria = confiteria;
+    }
+
+    }
+
+}
